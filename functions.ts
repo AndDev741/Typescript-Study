11d982b964677ab89c4613c2c491041a2435da78
AndDev <anddesenvolved@gmail.com>
2024-01-03 16:33:57 -0300

    functions in typescript

---
+++ functions.ts
@@ -0,0 +1,11 @@
+const sum = (x: number, y: number) => {
+    return x + y;
+}
+
+const value = sum(1, 2);
+
+
+const echo = (message: string): void => {
+    console.log(message);
+}
+echo("Echo is the best");
